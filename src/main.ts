de57792f14f138fab46a2b9117a85563ed83d0b4
Devanshu Jha <99939860+Devanshu254@users.noreply.github.com>
2024-05-25 09:44:45 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ async function bootstrap() {
   app.useGlobalPipes(
     new ValidationPipe()
   );
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT, '0.0.0.0');
 }
 bootstrap();
